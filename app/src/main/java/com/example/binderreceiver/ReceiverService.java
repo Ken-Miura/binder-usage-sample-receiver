ae1f1ef7853a6b29780c7c82a18ef16d8727a58d
kmiura <ken.miura1102@gmail.com>
2019-06-13 23:45:25 +0900

    Succeeded receiving long and string

--- app/src/main/java/com/example/binderreceiver/ReceiverService.java
+++ app/src/main/java/com/example/binderreceiver/ReceiverService.java
@@ -2,10 +2,12 @@ package com.example.binderreceiver;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.widget.Toast;
 
 public final class ReceiverService extends Service {
 
@@ -26,7 +28,10 @@ public final class ReceiverService extends Service {
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case MSG_FROM_SENDER_SERVICE:
-                    // TODO deal with data
+                    final Bundle bundle = msg.getData();
+                    final long l = bundle.getLong("long");
+                    final String s = bundle.getString("str");
+                    Toast.makeText(ReceiverService.this, "long: " + l + ", str: " + s, Toast.LENGTH_LONG).show();
                     break;
                 default:
                     super.handleMessage(msg);
